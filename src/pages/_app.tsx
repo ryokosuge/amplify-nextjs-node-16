0d44ac35ccde503c7f2e537baf1ca70511558794
ryokosuge <ryo.kosuge@gmail.com>
2022-01-27 14:42:39 +0900

    feat:	polaris

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
+import '@shopify/polaris/build/esm/styles.css';
 import type { AppProps } from 'next/app'
+import { AppProvider } from "@shopify/polaris";
+import translations from "@shopify/polaris/locales/en.json";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppProvider i18n={translations}>
+      <Component {...pageProps} />
+    </AppProvider>
+  );
 }
 
 export default MyApp
